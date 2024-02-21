e055455804442a5ade14624868aafea7ae4cc4c0
v1nc33n7 <v1nc33n7@proton.me>
2024-02-21 21:29:20 +0100

    interactive button

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -3,6 +3,9 @@ window.addEventListener('load', function(event) {
   const loginPage = document.getElementById('login');
   const chatPage = document.getElementById('chat');
 
+  const msgBox = document.getElementById('message');
+  const sendButton = document.getElementById('send');
+
   const response = function(message) {
     const d = document.createElement('div');
     d.textContent = message;
@@ -66,21 +69,72 @@ window.addEventListener('load', function(event) {
     return false;
   };
 
-  document
-      .getElementById('message')
+  const disableBtn = function(b) {
+    b.style.color = 'rgb(82 82 82 / var(--tw-text-opacity))';
+    b.style.borderColor = 'rgb(23 23 23 / var(--tw-border-opacity))';
+    b.style.backgroundColor = 'rgb(38 38 38 / var(--tw-bg-opacity))';
+
+    b.style.cursor = 'default';
+  };
+
+  const enableBtn = function(b) {
+    b.style.color = 'rgb(255 255 255 / var(--tw-text-opacity))';
+    b.style.borderColor = 'rgb(127 29 29 / var(--tw-border-opacity))';
+    b.style.backgroundColor = 'rgb(153 27 27 / var(--tw-bg-opacity))';
+
+    b.style.cursor = 'pointer';
+  };
+
+  const hoverBtn = function(b) {
+    b.style.backgroundColor = 'rgb(220 38 38 / var(--tw-bg-opacity))';
+  };
+
+  const unhoverBtn = function(b) {
+    b.style.backgroundColor = 'rgb(153 27 27 / var(--tw-bg-opacity))';
+  };
+
+  msgBox
       .addEventListener('keypress', function(event) {
         if (event.key === 'Enter') {
-          document.getElementById('send').click();
+          if (msgBox.value == '') {
+            return false;
+          }
+
+          sendButton.click();
         }
       });
 
-  document.getElementById('send').onclick = function(event) {
+  msgBox
+      .addEventListener('input', function(event) {
+        if (msgBox.value != '') {
+          enableBtn(sendButton);
+        } else {
+          disableBtn(sendButton);
+        }
+      });
+
+  sendButton
+      .addEventListener('mouseover', function(event) {
+        if (msgBox.value != '') {
+          hoverBtn(sendButton);
+        }
+      });
+
+  sendButton
+      .addEventListener('mouseleave', function(event) {
+        if (msgBox.value != '') {
+          unhoverBtn(sendButton);
+        }
+      });
+
+  sendButton.onclick = function(event) {
     if (!ws) {
       return false;
     }
-    ws.send(document.getElementById('message').value);
+    ws.send(msgBox.value);
 
-    document.getElementById('message').value = '';
+    msgBox.value = '';
+    disableBtn(sendButton);
 
     return false;
   };
